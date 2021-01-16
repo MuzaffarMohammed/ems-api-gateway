23657b26ee929ba6f18d49d7b36443bad85cfab2
Muzaffar Mohammed <50576677+MuzaffarMohammed@users.noreply.github.com>
2021-01-16 11:46:35 +0530

    Initial setup added

---
+++ src/test/java/com/appseonit/ems/apigateway/EmsZuulProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appseonit.ems.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsZuulProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
